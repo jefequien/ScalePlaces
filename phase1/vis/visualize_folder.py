57048a0992bfa25de3a181aec12c3a56dda11fc8
Jeffrey Hu <hujh@mit.edu>
2017-06-13 19:35:09 -0400

    visualize all folders

--- phase1/vis/visualize_folder.py
+++ phase1/vis/visualize_folder.py
@@ -1,26 +1,36 @@
 import sys
 import os
 
+def generateHTML(folder_path):
+    folder_name = os.path.basename(os.path.normpath(folder_path))
+
+    num = 0
+    max_num = 1000
+    body = "<body>"
+    for filename in os.listdir(folder_path):
+        if ".jpg" in filename or ".png" in filename:
+            image_path = os.path.join(folder_path, filename)
+            image_tag = "<img src=\"{}\" height=\"256px\">".format(image_path)
+            body += "<br><br> {} <br> {}".format(image_path, image_tag)
+            num += 1
+            if num == max_num:
+                body += "</body>"
+                break
+
+    html = "<html><head></head>{}</html>".format(body)
+
+    output_file = "{}.html".format(folder_name)
+    with open(output_file, 'w') as f:
+        f.write(html)
+
+    print "http://places.csail.mit.edu/scaleplaces/ScalePlaces/phase1/vis/{}".format(output_file)
+
+
 folder_path = sys.argv[1]
-folder_name = os.path.basename(os.path.normpath(folder_path))
+# generateHTML(folder_path)
 
-num = 0
-max_num = 1000
-body = "<body>"
-for filename in os.listdir(folder_path):
-    if ".jpg" in filename or ".png" in filename:
-        image_path = os.path.join(folder_path, filename)
-        image_tag = "<img src=\"{}\" height=\"256px\">".format(image_path)
-        body += "<br><br> {} <br> {}".format(image_path, image_tag)
-        num += 1
-        if num == max_num:
-            body += "</body>"
-            break
+for im_folder in os.listdir(folder_path):
+    if os.isdir(im_folder):
+        im_folder_path = os.path.join(folder_path, im_folder)
+        generateHTML(im_folder_path)
 
-html = "<html><head></head>{}</html>".format(body)
-
-output_file = "{}.html".format(folder_name)
-with open(output_file, 'w') as f:
-    f.write(html)
-
-print "http://places.csail.mit.edu/scaleplaces/ScalePlaces/phase1/vis/{}".format(output_file)
