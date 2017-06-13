261e30fbd195c0b56e919ec02871cda5e5a08bbf
Jeffrey Hu <hujh@mit.edu>
2017-06-13 15:11:56 -0400

    vis

---
+++ phase1/vis/visualize_folder.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+folder_path = sys.argv[1]
+folder_name = os.path.basename(os.path.normpath(folder_path))
+
+num = 0
+max_num = 1000
+body = "<body>"
+for filename in os.listdir(folder_path):
+    if ".jpg" in filename or ".png" in filename:
+        image_path = os.path.join(folder_path, filename)
+        image_tag = "<img src=\"{}\" height=\"256px\">".format(image_path)
+        body += "<br><br> {} <br> {}".format(image_path, image_tag)
+        num += 1
+        if num == max_num:
+            body += "</body>"
+            break
+
+html = "<html><head></head>{}</html>".format(body)
+
+print html
+
+with open("{}.html".format(folder_name), 'w') as f:
+    f.write(html)
