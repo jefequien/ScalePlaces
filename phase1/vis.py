ffe7ee16b554ce563fee15a855b104c1832dbe5b
Jeffrey Hu <hujh@mit.edu>
2017-06-21 15:55:23 -0400

    working diff

--- phase1/vis.py
+++ phase1/vis.py
@@ -1,72 +1,41 @@
 import argparse
 import os
+import random
 import uuid
-
-from scipy import misc
-import numpy as np
+import time
 
 import utils
-import time
-import random
+from vis_image import ImageVisualizer
 
 class Visualizer:
-    def __init__(self, output_dir="tmp/"):
+    def __init__(self, project, output_dir):
+        self.project = project
+        self.image_visualizer = ImageVisualizer(project)
+        self.images_dir = self.image_visualizer.images_dir
+
         self.output_dir = output_dir
-        self.images_dir = "tmp/images/"
-        if not os.path.exists(self.images_dir):
-            os.makedirs(self.images_dir)
+        self.max_images = 100
 
-        self.max = 100
-
-    def makeImageSection(self, project, im):
-        html = "{} {}<br><br>".format(project, im)
-
-        # Paths
-        config = utils.get_data_config(project)
-        root_images = config["images"]
-        root_category_mask = os.path.join(config["pspnet_prediction"], "category_mask")
-        root_prob_mask = os.path.join(config["pspnet_prediction"], "prob_mask")
-        root_ground_truth = config["ground_truth"]
-
-        image = os.path.join(root_images, im)
-        category_mask = os.path.join(root_category_mask, im.replace('.jpg','.png'))
-        prob_mask = os.path.join(root_prob_mask, im)
-
-        try:
-            color_mask = self.createColorMask(category_mask)
-        except:
-            color_mask = None
-        try:
-            gt_path = os.path.join(root_ground_truth, im.replace('.jpg','.png'))
-            ground_truth = self.createColorMask(gt_path)
-        except:
-            ground_truth = None
-        try:
-            gt_path = os.path.join(root_ground_truth, im.replace('.jpg','.png'))
-            diff = self.createDiffImage(category_mask, gt_path)
-            error = self.createColorMask(diff)
-        except:
-            error = None
-
-        paths = [image,prob_mask,color_mask, ground_truth, error]
-        for path in paths:
-            html += self.getImageTag(path)
-
-        html += "<br><br>"
-        return html
-
-    def makeHTML(self, project, im_list, fname=None):
-        sections = ""
+    def makeHTML(self, im_list, fname=None):
+        img_sections = ""
         cnt = 0
-        for im in im_list:
+        for im in im_list[:self.max_images]:
+            img_section = "<br><br>{} {}<br><br>".format(self.project, im)
+
+            im = im.split()[0]
+            paths = self.image_visualizer.visualize(im)
+            img_section += self.getImageTag(paths["image"])
+            img_section += self.getImageTag(paths["prob_mask"])
+            img_section += self.getImageTag(paths["category_mask"])
+            img_section += self.getImageTag(paths["ground_truth"])
+            img_section += self.getImageTag(paths["diff"])
+
+            img_sections += img_section
+
             print cnt, im
-            sections += self.makeImageSection(project, im)
-
             cnt += 1
-            if cnt == self.max:
-                break
 
-        body = "<body>{}</body>".format(sections)
+        body = "<body>{}</body>".format(img_sections)
         html = "<html><head></head>{}</html>".format(body)
 
         if not fname:
@@ -76,24 +45,6 @@ class Visualizer:
         with open(output_path, 'w') as f:
             f.write(html)
         return output_path
-
-    def createDiffImage(self, path, gt_path):
-        pred = misc.imread(path)
-        gt = misc.imread(gt_path)
-        gt[gt==pred] = 0
-        return gt
-
-    def createColorMask(self, category_mask_path):
-        category_mask = misc.imread(category_mask_path)
-        h,w = category_mask.shape
-        color_mask = np.zeros((h,w,3))
-        for i in xrange(1,151):
-            color_mask[category_mask == i] = utils.to_color(i)
-
-        fn = "{}.jpg".format(uuid.uuid4().hex)
-        path = os.path.join(self.images_dir, fn)
-        misc.imsave(path, color_mask)
-        return path
 
     def getImageTag(self, path):
         if path:
@@ -111,32 +62,26 @@ class Visualizer:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", required=True, help="Project name")
-    parser.add_argument("-i", help="Image name")
     parser.add_argument("--imlist", help="Image list")
-    parser.add_argument("--output", help="Output directory")
+    parser.add_argument("-n", default=100, help="Number of images")
+    parser.add_argument("-o", default="tmp/", help="Output directory")
     args = parser.parse_args()
 
     project = args.p
-    im = args.i
-    im_list_file = args.imlist
-    output_dir = args.output
+    n = int(args.n)
+    output_dir = args.o
 
     im_list = []
-    if im:
-        im_list = [im]
-    elif im_list_file:
-        im_list = [line.split()[0] for line in open(im_list_file, 'r')]
+    if args.imlist:
+        im_list = [line for line in open(args.imlist, 'r')]
     else:
         config = utils.get_data_config(project)
-        im_list_file = config["im_list"]
-        if im_list_file:
-            im_list = [line.rstrip() for line in open(im_list_file, 'r')]
-            random.shuffle(im_list)
+        im_list = [line for line in open(config["im_list"], 'r')]
+        random.shuffle(im_list)
+    im_list = im_list[:n]
 
-    vis = Visualizer()
-    if output_dir:
-        vis = Visualizer(output_dir=output_dir)
+    vis = Visualizer(project, output_dir)
 
-    output_path = vis.makeHTML(project, im_list)
+    output_path = vis.makeHTML(im_list)
     print "http://places.csail.mit.edu/scaleplaces/ScalePlaces/phase1/{}".format(output_path)
 
---
+++ phase1/vis_image.py
@@ -0,0 +1,111 @@
+import argparse
+import os
+import uuid
+
+from scipy import misc
+import numpy as np
+
+import utils
+
+class ImageVisualizer:
+
+    def __init__(self, project):
+        self.images_dir = "tmp/images/"
+        if not os.path.exists(self.images_dir):
+            os.makedirs(self.images_dir)
+
+        config = utils.get_data_config(project)
+        self.root_images = config["images"]
+        self.root_cm = os.path.join(config["pspnet_prediction"], "category_mask")
+        self.root_pm = os.path.join(config["pspnet_prediction"], "prob_mask")
+        self.root_gt = config["ground_truth"]
+
+    def visualize(self, im):
+
+        im_path = os.path.join(self.root_images, im)
+        cm, cm_path = self.get_category_mask(im)
+        pm, pm_path = self.get_prob_mask(im)
+        gt, gt_path = self.get_ground_truth(im)
+
+        cm_color, cm_color_path = self.add_color(cm)
+        gt_color, gt_color_path = self.add_color(gt)
+
+        diff = self.get_diff(cm, gt)
+        diff_color, diff_color_path = self.add_color(diff)
+
+        paths = {}
+        paths["image"] = im_path
+        paths["category_mask"] = cm_color_path
+        paths["prob_mask"] = pm_path
+        paths["ground_truth"] = gt_color_path
+        paths["diff"] = None
+        return paths
+
+    def get_category_mask(self, im):
+        try:
+            path = os.path.join(self.root_cm, im.replace('.jpg','.png'))
+            img = misc.imread(path)
+            return img, path
+        except:
+            return None, None
+
+    def get_prob_mask(self, im):
+        try:
+            path = os.path.join(self.root_pm, im)
+            img = misc.imread(path)
+            return img, path
+        except:
+            return None, None
+
+    def get_ground_truth(self, im):
+        try:
+            path = os.path.join(self.root_gt, im.replace('.jpg','.png'))
+            img = misc.imread(path)
+            return img, path
+        except:
+            return None, None
+
+    def get_diff(self, cm, gt):
+        if cm is None or gt is None:
+            return None
+        mask = gt - cm
+        mask.astype(bool)
+        diff = np.copy(gt)
+        diff[mask] = 0
+        return diff
+
+    def add_color(self, img):
+        if img is None:
+            return None, None
+
+        h,w = img.shape
+        img_color = np.zeros((h,w,3))
+        for i in xrange(1,151):
+            img_color[img == i] = utils.to_color(i)
+        path = self.save(img_color)
+        return img_color, path
+
+    def save(self, img):
+        fname = "{}.jpg".format(uuid.uuid4().hex)
+        path = os.path.join(self.images_dir, fname)
+        misc.imsave(path, img)
+        return path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", required=True, help="Project name")
+    parser.add_argument("-i", help="Image name")
+    args = parser.parse_args()
+
+    project = args.p
+    im = args.i
+    if not im:
+        f = utils.get_data_config(project)["im_list"]
+        im_list = [line.rstrip() for line in open(f, 'r')]
+        im = im_list[0]
+
+    print project, im
+    vis = ImageVisualizer(project)
+    paths = vis.visualize(im)
+    print paths
+
