0985601bb75cb4788b0b07de159528f8a2e5326c
Jeffrey Hu <hujh@mit.edu>
2017-06-23 12:49:10 -0400

    eval categories

---
+++ phase1/eval_categories.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+import numpy as np
+import h5py
+
+import utils
+
+def open_data(fname, dataset_name):
+    with h5py.File('fname', 'r') as f:
+        output = f[dataset_name]
+        return output[:]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", required=True, help="Date file")
+args = parser.parse_args()
+
+fname = args.f
+data = open_data(fname, "accuracies")
+averages = np.nanmean(data, axis=0)
+
+categories = utils.get_categories()
+for i in xrange(len(averages)):
+    avg = averages[i]
+    avg = "%.3f" % avg
+    c = i+1
+    output = "{} {} {}".format(c, categories[c], avg)
+    print output
