4ba64c51bdf19a55e7b4aae50905f0f1b94a1c9d
Jeffrey Hu <hujh@mit.edu>
2017-06-29 12:01:53 -0400

    eval_h5.py

--- phase1/eval/eval_h5.py
+++ phase1/eval/eval_h5.py
@@ -9,18 +9,20 @@ def open_data(fname, dataset_name):
     with h5py.File(fname, 'r') as f:
         output = f[dataset_name]
         return output[:]
-def eval_data(data):
+def eval_data(data, output_file):
     averages = np.nanmean(data, axis=0)
     nonzero = ~np.isnan(data)
     counts = np.sum(nonzero, axis=0)
 
+    output = ""
     categories = utils.get_categories()
     for i in xrange(150):
         avg = averages[i]
         count = counts[i]
         c = i + 1
-        output = "{} {} {} {}".format(c, categories[c], avg, count)
-        print output
+        output += "{} {} {} {}\n".format(c, categories[c], avg, count)
+    with open(output_file, 'w') as f:
+        f.write(output)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", required=True, help="Date file")
@@ -32,5 +34,9 @@ recall = open_data(fname, "recall")
 iou = open_data(fname, "iou")
 print precision.shape, recall.shape, iou.shape
 
-eval_data(iou)
-
+precision_fname = "precision/{}".format(fname.replace(".h5", ".txt"))
+eval_data(precision, precision_fname)
+recall_fname = "recall/{}".format(fname.replace(".h5", ".txt"))
+eval_data(recall, recall_fname)
+iou_fname = "iou/{}".format(fname.replace(".h5", ".txt"))
+eval_data(iou, iou_fname)
