3b3be8bc0b64e4bfcd91a13d43b933215dbaa41d
Jeffrey Hu <hujh@mit.edu>
2017-07-06 15:55:21 -0400

    old process

--- phase1/run/old_process.py
+++ phase1/run/old_process.py
@@ -1,6 +1,12 @@
+import numpy as np
+import itertools
+from scipy import misc, ndimage
 
+DATA_MEAN = np.array([[[123.68, 116.779, 103.939]]])
+INPUT_SIZE = 473
+NUM_CLASS = 150
 
-def process(self, image):
+def process(image):
     localtime = time.asctime(time.localtime(time.time()))
 
     if image.ndim != 3:
