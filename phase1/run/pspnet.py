c347ba3a49c09bcd80aaab8cc50fe0f038ba6c07
Jeffrey Hu <hujh@mit.edu>
2017-07-06 17:13:18 -0400

    mistake

--- phase1/run/pspnet.py
+++ phase1/run/pspnet.py
@@ -68,7 +68,7 @@ class PSPNet:
         crop_boxes = pspnet_utils.get_crop_boxes(h,w)
         for i in xrange(n):
             sh,eh,sw,ew = crop_boxes[i]
-
+            out = outs[i]
 
             cnts[0,sh:eh,sw:ew] += 1
             probs[:,sh:eh,sw:ew] += out[:,0:eh-sh,0:ew-sw]
