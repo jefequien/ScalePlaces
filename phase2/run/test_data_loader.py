ab6949d1cc2114ad027fbd5a05aa890256345e32
Jeffrey Hu <hujh@mit.edu>
2017-07-17 16:19:40 -0400

    prefetcher

--- phase2/run/test_data_loader.py
+++ phase2/run/test_data_loader.py
@@ -1,14 +1,14 @@
 
 import utils_run as utils
 from data_source import DataSource
-from data_loader import DataLoader
+from prefetcher import PreFetcher
 
 
 project = "local"
 config = utils.get_config(project)
 
 data_source = DataSource(config, random=True)
-data_loader = DataLoader(data_source, batch_size=5, ahead=12)
+prefetcher = PreFetcher(data_source, batch_size=5, ahead=12)
 
-data, label = data_loader.fetch_batch()
+data, label = prefetcher.fetch_batch()
 print data.shape, label.shape
